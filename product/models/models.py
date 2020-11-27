d8635e6d327bb40352623303d6946f7b58fd2db1
ismailaslanbay <ismailaslanbay@gmail.com>
2020-11-27 14:45:49 +0300

    cretaed product models.py

---
+++ product/models/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name    = "Urun"
+        verbose_name_plural    = "Urunler"
+
